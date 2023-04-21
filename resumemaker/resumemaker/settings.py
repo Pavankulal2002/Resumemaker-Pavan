0a97ca9f256d5e43245d1516bec6d19f48b114ea
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2023-04-21 14:03:45 +0530

    *

--- resumemaker/resumemaker/settings.py
+++ resumemaker/resumemaker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(ck6u=%%edf%$n2kizgy0-vkaxll2$r!#2*g3p-afgsq39ld9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
