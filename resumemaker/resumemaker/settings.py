e2800ad5feea44671ff3c9109bb54010d4c6ef7e
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2023-04-22 16:50:40 +0530

    ..

--- resumemaker/resumemaker/settings.py
+++ resumemaker/resumemaker/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ck6u=%%edf%$n2kizgy0-vkaxll2$r!#2*g3p-afgsq39ld9x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
